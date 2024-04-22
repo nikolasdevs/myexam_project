678a58c2dbf47f836f5a1e5c520bb334e482300b
Emmanuel Esan <nikolasdevs@gmail.com>
2024-04-22 22:47:00 +0100

    i created home link for the logo and added padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import logo from "../e-logo.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div>
-      <img src={logo} alt="logo" className=" w-16 h-16 my-3 mx-auto" />
-      
+      {" "}
+      <Link to="/">
+        <img src={logo} alt="logo" className=" w-16 h-16 my-3 mx-auto" />
+      </Link>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,36 +2,22 @@ import React from "react";
 import logo from "../e-logo.svg";
 
 import { Link } from "react-router-dom";
-//import RepoList from "./RepoList";
-//import RepoData from "./RepoData";
 
 export default function Home() {
   return (
     <>
-      {/* <Header /> */}
-      <div className="App-header mx-auto">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className=" text-8xl font-black">
-        Welcome to my Repository Page
+      <div className="App-header mx-auto md:px-56 px-24 flex items-center">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+        <p className=" md:text-6xl text-6xl  font-black">
+          Welcome to my Repository Page
         </p>
 
         <Link to="./RepoList">
-          <button className="  py-4 px-16 rounded-full  ">ENTER</button>
+          <button className="  py-4 px-24 rounded-full  mb-16">ENTER</button>
         </Link>
       </div>
     </>
   );
 }
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Routes>
-//         <Route exact path="/" element={<RepoList />} />
-//         <Route path="/repo/:repoName" element={<RepoData />} />
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default Home;
